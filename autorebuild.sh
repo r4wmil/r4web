89ff502b133be04887f9698f84ed6c7c957613f7
Ramil Khasanshin <khasanshin.ramil@list.ru>
2026-03-04 12:39:14 +0300

    chore: returned autorebuild.sh

---
+++ autorebuild.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+ENTR_CMD="./build && ./out/r4web $@"
+
+find . -type f \
+	-name "*.c" -o \
+	-name "*.h" -o \
+	-name "Makefile" -o \
+	-name "_*.html" \
+	| entr -r bash -c "$ENTR_CMD"
